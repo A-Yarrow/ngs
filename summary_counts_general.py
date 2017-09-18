a3c638a986970d4bc4a14d552afadaf73d03295e
Yarrow Madrona <yarrowmadrona@gmail.com>
2017-09-18 16:04:54 -0700

    output is now .txt file instead of .csv

---
+++ summary_counts_general.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import pandas as pd
+#import unicodecsv
+import os
+
+def get_data():
+    df_sum = pd.DataFrame()
+    for (dirname, dirs, files) in os.walk('.'):
+        for filename in files:
+            filename = filename.lower()
+            if filename.endswith('.csv'):
+                df = pd.read_csv(filename, skiprows=1, header=0, names = ['genes', filename+'_counts'])
+                #print df
+                df_indexed = df.set_index(df['genes'])
+                del df_indexed['genes']
+                #print df_indexed
+                df_sum = pd.concat([df_sum, df_indexed], axis=1)
+        df_sum.to_csv('compiled_gene_counts.txt', na_rep = 'NA' )
+        print df_sum
+get_data()
